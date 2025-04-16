3952fa1c3dce4ecef8e3a23f55a1fc14ef1fae1a
anjanasilva99 <anjana@shiftgroup.ca>
2025-04-16 11:17:40 +0530

    Add react-icons and tailwindcss dependencies; update devDependencies

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// filepath: d:\cogentsolution\finastra-landing-page\postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0c2e60',
+        secondary: '#f4802a',
+      }
+    },
+  },
+  plugins: [],
+}
